72391e87319e223c1cdff20c9f72c885edce0408
Saha-Ovi <ovisaha3210@gmail.com>
2026-03-31 22:50:21 +0600

    navbar cart indicator add

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { IoCartOutline } from 'react-icons/io5';
 import navImg from '../../assets/products/DigiTools.png'
 
-const Navbar = ({cart}) => {
+const Navbar = ({ cart }) => {
     return (
         <div className='container mx-auto my-5'>
             <div className="navbar bg-base-100 shadow-sm p-4">
@@ -21,7 +21,7 @@ const Navbar = ({cart}) => {
                             <li className='text-[#101727] font-semibold'><a>FAQ</a></li>
                         </ul>
                     </div>
-                        <a href=""><img src={navImg} alt="Navbar Image" /></a>
+                    <a href=""><img src={navImg} alt="Navbar Image" /></a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
@@ -32,13 +32,15 @@ const Navbar = ({cart}) => {
                         <li className='text-[#101727] font-semibold'><a>FAQ</a></li>
                     </ul>
                 </div>
-                <div className="navbar-end gap-2 lg:gap-5">
-                    <a className='text-xl lg:text-2xl text-[#101727] hover:cursor-pointer' >
-                        <span className='flex items-center gap-1'>
-                            <IoCartOutline />
-                            <p className=' text-zinc-500'>({cart.length})</p>
-                        </span>
-                    </a>
+                <div className="navbar-end gap-4 lg:gap-5">
+                    <div class="indicator">
+                        <span class="indicator-item badge badge-xs lg:badge-sm badge-secondary">{cart.length}</span>
+                        <a className='text-xl lg:text-3xl text-[#101727] hover:cursor-pointer' >
+                            <span className='flex items-center gap-1'>
+                                <IoCartOutline />
+                            </span>
+                        </a>
+                    </div>
                     <a className="btn text-[#101727] rounded-full font-semibold">Login</a>
                     <a className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white rounded-full font-semibold hidden lg:inline-flex">Get Started</a>
                 </div>
@@ -46,5 +48,7 @@ const Navbar = ({cart}) => {
         </div>
     );
 };
+
+
 
 export default Navbar;
